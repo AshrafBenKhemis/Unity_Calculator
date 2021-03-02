f60dc53f2ccc0745d9d5e2fc550b150a6aae3ee7
Ashraf Ben Khemis <47083271+AshrafBenKhemis@users.noreply.github.com>
2021-03-02 15:41:06 +0100

    Add files via upload

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    public InputField inputField1;
+    public InputField inputField2;
+    public Text resultText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultText.text = "0";
+    }
+    public void Add()
+    {
+        int number1 = int.Parse(inputField1.text);
+        int number2 = int.Parse(inputField2.text);
+        resultText.text = (number1 + number2).ToString();
+    }
+    public void Sub()
+    {
+        int number1 = int.Parse(inputField1.text);
+        int number2 = int.Parse(inputField2.text);
+        resultText.text = (number1 - number2).ToString();
+    }
+    public void Div()
+    {
+        int number1 = int.Parse(inputField1.text);
+        int number2 = int.Parse(inputField2.text);
+        resultText.text = (number1 / number2).ToString();
+    }
+    public void Mult()
+    {
+        int number1 = int.Parse(inputField1.text);
+        int number2 = int.Parse(inputField2.text);
+        resultText.text = (number1 * number2).ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
